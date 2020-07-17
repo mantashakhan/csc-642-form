c60917d894337c234bff2e5c7c0342d95ddd6d58
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-07-17 07:50:45 +0500

    ...

--- public/index.js
+++ public/index.js
@@ -24,6 +24,39 @@ const load = () => {
     } else {
       document.getElementById("submission").classList += " hide";
       document.getElementById("submitted").classList = "card-body";
+      document.getElementById("results").classList = "mt-3 card";
+
+      document.getElementById("firstnameResult").innerHTML =
+        "Firstname: " + document.getElementById("firstname").value;
+
+      document.getElementById("lastnameResult").innerHTML =
+        "Lastname: " + document.getElementById("lastname").value;
+
+      document.getElementById("addressResult").innerHTML =
+        "Address: " + document.getElementById("address").value;
+
+      document.getElementById("dobResult").innerHTML =
+        "Date of Birth: " + document.getElementById("dob").value;
+
+      document.getElementById("eduResult").innerHTML =
+        "Education Level: " + document.getElementById("edu").value;
+      if (
+        document.getElementById("feet").value !== "" &&
+        document.getElementById("inches").value !== ""
+      ) {
+        document.getElementById("heightResult").innerHTML =
+          "Height: " +
+          document.getElementById("feet").value +
+          " feet and " +
+          document.getElementById("inches").value +
+          " inches";
+      }
+
+      document.getElementById("phoneResult").innerHTML =
+        "Phone: " + document.getElementById("phone").value;
+
+      document.getElementById("emailResult").innerHTML =
+        "Email: " + document.getElementById("email").value;
     }
   };
 };
